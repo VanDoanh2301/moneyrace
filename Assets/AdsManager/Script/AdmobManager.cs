7ab44840c2a086882f081992ea175fc07629937c
DoanhMac <doanhtv2759@gmail.com>
2026-03-02 09:23:15 +0700

    remove ads

--- Assets/AdsManager/Script/AdmobManager.cs
+++ Assets/AdsManager/Script/AdmobManager.cs
@@ -1,18 +1,13 @@
-﻿#if USE_ADMOB
-using GoogleMobileAds.Api;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using UnityEngine;
 
+/// <summary>
+/// Stub: Ads removed from app. Kept for compatibility with existing scenes/prefabs.
+/// </summary>
 public class AdmobManager : MonoBehaviour
 {
     public static AdmobManager instance;
-    public AdmobOpenApp openApp;
-    public AdmobBanner banner;
-    public AdmobInter inter;
-    public AdmobReward reward;
-    public bool isShowBanner;
+
     void Start()
     {
         if (instance == null)
@@ -24,27 +19,15 @@ public class AdmobManager : MonoBehaviour
         {
             Destroy(this);
         }
-        // Initialize the Google Mobile Ads SDK.
-        MobileAds.Initialize(initStatus => { });
-        if (isShowBanner)
-            banner.LoadAd();
-        Invoke("DelayShowAppOpen", 3f);
     }
 
-    void DelayShowAppOpen()
-    {
-        openApp.LoadAd();
-    }
     public void ShowAdReward(Action complete, Action fail)
     {
-#if UNITY_EDITOR
         complete?.Invoke();
-#endif
-        reward.ShowRewardedAd(complete, fail);
     }
+
     public void ShowAdInter()
     {
-        inter.ShowAd();
+        // No-op: ads removed
     }
 }
-#endif
